fe19f96171af5e52e14db788617444dda1f32f92
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2024-12-27 16:20:10 +0900

    feat: initial commit

---
+++ src/main/java/moe/nmkmn/player_home/PlayerHome.java
@@ -0,0 +1,17 @@
+package moe.nmkmn.player_home;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerHome extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
