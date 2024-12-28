aa8e8d53793c89e83bd5e7f0f916149a669573bf
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2024-12-28 12:08:28 +0900

    feat: データベースの接続を追加

--- src/main/java/moe/nmkmn/player_home/PlayerHome.java
+++ src/main/java/moe/nmkmn/player_home/PlayerHome.java
@@ -1,18 +1,47 @@
 package moe.nmkmn.player_home;
 
+import moe.nmkmn.player_home.commands.HomeCommand;
 import moe.nmkmn.player_home.commands.SethomeCommand;
+import moe.nmkmn.player_home.listeners.SethomeListener;
+import moe.nmkmn.player_home.utils.DatabaseManager;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.SQLException;
 import java.util.Objects;
 
 public final class PlayerHome extends JavaPlugin {
     public MiniMessage miniMessage = MiniMessage.miniMessage();
     public Component prefix = miniMessage.deserialize("<gray>[</gray><gradient:#44DAEA:#3163EB>PlayerHome</gradient><gray>]</gray> ");
 
+    public DatabaseManager databaseManager;
+
     @Override
     public void onEnable() {
-        Objects.requireNonNull(getCommand("sethome")).setExecutor(new SethomeCommand(this));
+        if (!getDataFolder().exists()) getDataFolder().mkdirs();
+
+        // データベースに接続
+        try {
+            databaseManager = new DatabaseManager(getDataFolder().getAbsolutePath() + "/database.db");
+        } catch (SQLException e) {
+            getLogger().severe("Failed to connect to database: " + e.getMessage());
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        getCommand("home").setExecutor(new HomeCommand(this));
+        getCommand("sethome").setExecutor(new SethomeCommand(this));
+        Bukkit.getPluginManager().registerEvents(new SethomeListener(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+        try {
+            databaseManager.close();
+        } catch (SQLException e) {
+            getLogger().severe("Failed to close to database: " + e.getMessage());
+        }
     }
 }
